8e4fddfe8d6a0d0fdbb4a8c85b2e160d95418f7b
aliyurekli <yurekli.ali@gmail.com>
2019-10-18 13:07:53 +0300

    ArrayList demonstration scenario is included in the corresponding class.

--- implementations/src/main/java/ceng/eskisehir/edu/tr/list/ArrayListDemo.java
+++ implementations/src/main/java/ceng/eskisehir/edu/tr/list/ArrayListDemo.java
@@ -3,79 +3,82 @@ package ceng.eskisehir.edu.tr.list;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+
+/**
+ *  ArrayList Demonstration Scenario
+ *
+ *  1. Declare an ArrayList to store famous car manufacturers
+ *  2. Add "Honda", "Toyota", "Audi", "BMW", "Renault" to the list
+ *  3. Print the total number of elements in the list
+ *  4. Iterate the list with a for loop and print the elements
+ *  5. Add "Ford" to the head of the list
+ *  6. Modify the last element of the list as "Hyundai"
+ *  7. Iterate the list with a for-each loop and print the elements
+ *  8. Search for "BMW" in the list, print the first index of the element if it exists
+ *  9. Iterate the list using an iterator and print the elements
+ *  10. Remove all the elements of the list
+ *  11. Check if the list is empty now
+ *
+ */
+
 public class ArrayListDemo implements Runnable {
 
-    /**
-     * When an object implementing interface <code>Runnable</code> is used
-     * to create a thread, starting the thread causes the object's
-     * <code>run</code> method to be called in that separately executing
-     * thread.
-     * <p>
-     * The general contract of the method <code>run</code> is that it may
-     * take any action whatsoever.
-     *
-     * @see Thread#run()
-     */
     @Override
     public void run() {
-        // Declare an ArrayList to store famous car manufacturers
+        // Step 1
         ArrayList<String> cars = new ArrayList<>();
 
-        // Add "Honda", "Toyota", "Audi", "BMW", "Renault" to the list
+        // Step 2
         cars.add("Honda");
         cars.add("Toyota");
         cars.add("Audi");
         cars.add("BMW");
         cars.add("Renault");
 
-        // Print the total number of elements in the list
+        // Step 3
         System.out.printf("There are %d elements in the list\n", cars.size());
 
-        // Iterate the list with a for loop and print the elements
+        // Step 4
         for (int i=0; i<cars.size(); i++) {
             System.out.println(cars.get(i));
         }
 
-        System.out.println("*******************************");
-
-        // Add "Ford" to the head of the list
+        // Step 5
         cars.add(0, "Ford");
 
-        // Modify the last element of the list as "Hyundai"
+        // Step 6
         cars.set(cars.size()-1, "Hyundai");
 
-        // Iterate the list with a for-each loop and print the elements
+        // Step 7
         for (String car : cars) {
             System.out.println(car);
         }
 
-        System.out.println("*******************************");
-
-        // Search for "BMW" in the list, print the first index of the element if it exists
-        if (cars.contains("BMW")) {
-            int firstIndex = cars.indexOf("BMW");
+        // Step 8
+        String bmw = "BMW";
+        if (cars.contains(bmw)) {
+            int firstIndex = cars.indexOf(bmw);
             System.out.printf("First index of BMW is %d\n", firstIndex);
         }
         else {
             System.out.println("BMW does not exist in the list");
         }
 
-        // Iterate the list using an iterator and print the elements
+        // Step 9
         Iterator<String> iterator = cars.iterator();
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
 
-        // Remove all elements of the list
+        // Step 10
         cars.clear();
 
-        // Check if the list is empty now
+        // Step 11
         boolean empty = cars.isEmpty();
         System.out.printf("The list is empty: %s", String.valueOf(empty));
     }
 
     public static void main(String[] args) {
-
         new Thread(new ArrayListDemo()).start();
     }
 }
